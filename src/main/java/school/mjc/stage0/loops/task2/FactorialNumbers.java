e88974d8ac366d3a8414bf1f471a8ae37d8b959c
jargr <jaroslav.grablevski@gmail.com>
2023-02-26 13:54:04 +0200

    While

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
 
+        int i = 0;
+        int f = 1;
+        while (i <= printToInclusive){
+            f = i <= 1? 1 : f * i;
+            System.out.println( f  );
+i++;
+        }
+
     }
 }
